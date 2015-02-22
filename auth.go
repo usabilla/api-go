e38aab850b1c25d466aa45f82c3ff9596df7214b
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2015-02-22 02:12:34 +0100

    Moved request methods to auth structure.

--- auth.go
+++ auth.go
@@ -38,3 +38,37 @@ func (au *auth) signature(sts, shortDate string) string {
 func (au *auth) signedHeaders() string {
 	return "date;host"
 }
+
+func (au *auth) payload(load string) string {
+	return hexHash([]byte(load))
+}
+
+func (au *auth) hashedCanonicalRequest(method, uri, query, rfcdate, host string) string {
+	return hexHash([]byte(au.canonicalRequest(method, uri, query, rfcdate, host)))
+}
+
+func (au *auth) stringToSign(method, uri, query, rfcdate, host, shortDate, shortDateTime string) string {
+	return fmt.Sprintf(
+		"%s\n%s\n%s\n%s",
+		algorithm,
+		shortDateTime,
+		au.credentialScope(shortDate),
+		au.hashedCanonicalRequest(method, uri, query, rfcdate, host),
+	)
+}
+
+func (au *auth) canonicalRequest(method, uri, query, rfcdate, host string) string {
+	return fmt.Sprintf(
+		"%s\n%s\n%s\n%s\n%s\n%s",
+		method,
+		uri,
+		query,
+		au.canonicalHeaders(rfcdate, host),
+		au.signedHeaders(),
+		au.payload(""),
+	)
+}
+
+func (au *auth) canonicalHeaders(rfcdate, host string) string {
+	return fmt.Sprintf("date:%s\nhost:%s\n", rfcdate, host)
+}
--- request.go
+++ request.go
@@ -40,9 +40,11 @@ func (r *Request) Get() ([]byte, error) {
 	request.Header.Add("date", rfcdate)
 	request.Header.Add("host", host)
 
-	request.URL.RawQuery = r.query()
+	query := r.query()
 
-	stringToSign := r.stringToSign(rfcdate, host, shortDate, shortDateTime)
+	request.URL.RawQuery = query
+
+	stringToSign := r.auth.stringToSign(r.method, r.uri, query, rfcdate, host, shortDate, shortDateTime)
 
 	request.Header.Add("authorization", r.auth.header(stringToSign, shortDate))
 
@@ -65,44 +67,10 @@ func (r *Request) url() string {
 	return fmt.Sprintf("%s://%s%s", scheme, host, r.uri)
 }
 
-func (r *Request) canonicalRequest(rfcdate, host string) string {
-	return fmt.Sprintf(
-		"%s\n%s\n%s\n%s\n%s\n%s",
-		r.method,
-		r.uri,
-		r.query(),
-		r.canonicalHeaders(rfcdate, host),
-		r.auth.signedHeaders(),
-		r.payload(),
-	)
-}
-
 func (r *Request) query() string {
 	v := url.Values{}
 	for key, value := range r.params {
 		v.Set(key, value)
 	}
 	return v.Encode()
-}
-
-func (r *Request) canonicalHeaders(rfcdate, host string) string {
-	return fmt.Sprintf("date:%s\nhost:%s\n", rfcdate, host)
-}
-
-func (r *Request) payload() string {
-	return hexHash([]byte(""))
-}
-
-func (r *Request) hashedCanonicalRequest(rfcdate, host string) string {
-	return hexHash([]byte(r.canonicalRequest(rfcdate, host)))
-}
-
-func (r *Request) stringToSign(rfcdate, host, shortDate, shortDateTime string) string {
-	return fmt.Sprintf(
-		"%s\n%s\n%s\n%s",
-		algorithm,
-		shortDateTime,
-		r.auth.credentialScope(shortDate),
-		r.hashedCanonicalRequest(rfcdate, host),
-	)
 }
