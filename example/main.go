f1479d103b9b9d3f7bd68f146dfe0c49a764666e
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2015-05-14 20:30:21 +0200

    Use normal GET and channel example only for buttons response. The rest resources are the same in terms of usage.

--- example/main.go
+++ example/main.go
@@ -16,8 +16,32 @@ func buttons(gb *gobilla.Gobilla) {
 	}
 
 	for _, button := range buttons.Items {
+		resp, err := b.Feedback().Get(button.ID, nil)
+		if err != nil {
+			fmt.Errorf("%s", err)
+		}
 		count := 0
-		fmt.Printf("START PRINTING FEEDBACK FROM BUTTON: %s\n", button.ID)
+		fmt.Printf("START PRINTING FEEDBACK FOR BUTTON: %s\n", button.ID)
+		for _, item := range resp.Items {
+			fmt.Printf("FEEDBACK %s\n", item.ID)
+			count++
+		}
+		fmt.Printf("RECEIVED %d FEEDBACK ITEMS\n", count)
+	}
+	fmt.Printf("RECEIVED FEEDBACK FROM %d BUTTONS\n", buttons.Count)
+}
+
+func buttonsIterator(gb *gobilla.Gobilla) {
+	b := gb.Buttons()
+
+	buttons, err := b.Get(nil)
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
+
+	for _, button := range buttons.Items {
+		count := 0
+		fmt.Printf("START PRINTING FEEDBACK FOR BUTTON: %s\n", button.ID)
 		for item := range b.Feedback().Iterate(button.ID, nil) {
 			fmt.Printf("FEEDBACK %s\n", item.ID)
 			count++
@@ -27,32 +51,16 @@ func buttons(gb *gobilla.Gobilla) {
 	fmt.Printf("RECEIVED FEEDBACK FROM %d BUTTONS\n", buttons.Count)
 }
 
-func campaigns(gb *gobilla.Gobilla) {
-	c := gb.Campaigns()
-
-	campaigns, err := c.Get(nil)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, campaign := range campaigns.Items {
-		count := 0
-		fmt.Printf("START PRINTING CAMPAIGN RESULTS FROM CAMPAIGN: %s\n", campaign.ID)
-		for result := range c.Results().Iterate(campaign.ID, nil) {
-			fmt.Printf("CAMPAIGN RESULT %s\n", result.ID)
-			count++
-		}
-		fmt.Printf("RECEIVED %d CAMPAIGN RESULTS\n", count)
-	}
-	fmt.Printf("RECEIVED CAMPAIGN RESULTS FROM %d CAMPAIGNS\n", campaigns.Count)
-}
-
 func main() {
 	args := os.Args[1:]
 
 	gb := gobilla.New(args[0], args[1])
 
+	// uses a simple GET to get all feedback items for all buttons
 	buttons(gb)
 
-	campaigns(gb)
+	// uses a channel of feedback items, once all items have been
+	// consumed and the response HasMore then it fires a new request
+	// for all feedback items for all buttons
+	buttonsIterator(gb)
 }
